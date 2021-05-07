142cb0cb86db6006a781ac79e2a280b58b89a22a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-07 18:05:28 -0300

    3 - Insira um link para a página de detalhes de um filme dentro de MovieCard ✅

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,17 +5,16 @@ import { Link } from 'react-router-dom';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, storyline, imagePath, id } = movie;
     return (
       <div className="movie-card" data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
         <div className="movie-card-body">
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
         <div className="movie-card-rating" data-testid="rating">
-          <Link to="/">VER DETALHES</Link>
+          <Link to={ `/movies/${id}` }>VER DETALHES</Link>
         </div>
       </div>
     );
@@ -29,6 +28,7 @@ MovieCard.propTypes = {
     storyline: PropTypes.string,
     rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     imagePath: PropTypes.string,
+    id: PropTypes.number,
   }).isRequired,
 };
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -39,7 +39,7 @@ class MovieList extends Component {
     }
     return (
       <div data-testid="movie-list" className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        { movies.map((movie) => <MovieCard key={ movie.id } movie={ movie } />) }
       </div>
     );
   }
